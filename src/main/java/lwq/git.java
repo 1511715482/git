0b6cd883143bf88c979a5a122c5114703fff0141
lwq <lwq@qq.com>
2023-01-02 22:46:20 +0800

    first2

--- src/main/java/lwq/git.java
+++ src/main/java/lwq/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("lwq");
+        System.out.println("lwq1");
     }
 }
