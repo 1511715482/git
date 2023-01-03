038a5826e090ca5261b31ac92eac7da9cad19910
lwq <lwq@qq.com>
2023-01-03 11:23:48 +0800

    hot-fix commit1

--- src/main/java/lwq/git.java
+++ src/main/java/lwq/git.java
@@ -4,7 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("lwq");
-        System.out.println("lwq1");
+        System.out.println("lwq");
         System.out.println("lwq2");
+        System.out.println("lwq3");
     }
 }
